785b8566c80e87c331880be232de9c79d9f22288
Jason Leocata <jasonleocata@Jasons-MacBook-Pro.local>
2017-08-01 18:35:17 +1000

    Added SWReveal functionality

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Jason Leocata on 1/8/17.
+//  Copyright © 2017 Jason Leocata. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+
+}
