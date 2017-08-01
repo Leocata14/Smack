78362d3966cb085df7f046b93869e789f0e3b5b2
Jason Leocata <jasonleocata@Jasons-MacBook-Pro.local>
2017-08-01 21:44:57 +1000

    Added code to register user and rounded buttons

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -9,13 +9,38 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
+    
+    
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-
+    @IBAction func createAccountButtonTapped(_ sender: Any) {
+        guard let email = emailText.text , emailText.text != "" else { return }
+        guard let pass = passwordText.text , passwordText.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func chooseAvatarButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func generateBGColourTapped(_ sender: Any) {
+    }
+    
     @IBAction func closeButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: SEGUE_UNWIND_TO_CHANNEL, sender: nil)
     }
