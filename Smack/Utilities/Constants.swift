d62dac31904119f2086032ea32ef37f7b4fd4e22
Jason Leocata <jasonleocata@Jasons-MacBook-Pro.local>
2017-08-01 21:24:05 +1000

    Created Auth Service for registration

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let URL_BASE = "https://leocata14-smack.herokuapp.com/v1/"
+let URL_REGISTER = "\(URL_BASE)account/register"
+
 //Seqgues
 let SEGUE_TO_LOGIN = "toLogin"
 let SEGUE_TO_CREATE_ACCOUNT = "toCreateAccount"
 let SEGUE_UNWIND_TO_CHANNEL = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
