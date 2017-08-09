c472afa0dc1b6dbaf4ea14cae11c54f8993802e7
Jason Leocata <jasonleocata@Jasons-MacBook-Pro.local>
2017-08-09 21:51:45 +1000

    Created table functions for channels

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var loginButton: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var userImage: CircleImage!
     var statusBarShouldBeHidden = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 100
         
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
@@ -34,6 +39,7 @@ class ChannelVC: UIViewController {
     @objc func userDataDidChange(_ notif: Notification) {
         setupUserInfo()
     }
+    
     
     func setupUserInfo() {
         if AuthService.instance.isLoggedIn {
@@ -64,6 +70,24 @@ class ChannelVC: UIViewController {
             performSegue(withIdentifier: SEGUE_TO_LOGIN, sender: nil)
         }
         
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return ChannelCell()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
     }
     
 }
